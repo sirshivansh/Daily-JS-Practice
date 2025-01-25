88e1086afe4893cf39f7eb7de6a1e55714af0e4b
Shivansh Mishra <shivnsh01@gmail.com>
2025-01-26 00:45:06 +0530

    Creating an Object, but displaying with limited access

---
+++ cr8_obj_limited_access.js
@@ -0,0 +1,10 @@
+// Given an object student = { name: "Alice", age: 20, grade: "A" }
+// ,access the name and grade properties and print them to the console.
+
+let student = {
+    name: "Alice",
+    age: 20,
+    grade: "A"
+};
+console.log(student["name"]);
+console.log(student["grade"]);
