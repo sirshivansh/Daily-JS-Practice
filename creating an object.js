4497a03fa77c75b2ceb2540bbee91439ff0ab5d2
Shivansh Mishra <shivnsh01@gmail.com>
2025-01-26 00:19:36 +0530

    Creating an Object, and rendering the output!

---
+++ creating an object.js
@@ -0,0 +1,12 @@
+// Creating an Object
+/*Create an object book that contains the following properties:
+title, author, and yearPublished. Assign them appropriate values 
+(e.g., title: "The Great Gatsby", author: "F. Scott Fitzgerald", yearPublished: 1925).*/
+
+let book = {
+    title : "No Longer Human",
+    author : "Osamu Dazai",
+    yearPublished : 1948
+};
+
+console.log(book);
