7070812c5e082c947e85bf913acdba115bece6fd
Shivansh Mishra <shivnsh01@gmail.com>
2025-01-26 02:21:55 +0530

    Adding New Properties in Object!

--- add_new_properties_in_obj.js
+++ add_new_properties_in_obj.js
@@ -1,9 +1,13 @@
 // Add a new property city to the student object from the previous question,
 // and set it to "New York". Print the object to check if the new property is added.
+// Modify the age property of the student object to 21 and print the updated object.
 let student = {
     name: "Alice",
     age: 20,
     grade: "A",
 };
 student.city = "New York";
+student.age = 21;
+
+
 console.log(student);
